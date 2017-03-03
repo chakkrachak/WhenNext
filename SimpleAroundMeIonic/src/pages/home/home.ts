2cc46d7ed246ed05a0c415cce4c21f9f2510545e
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-03-03 12:21:02 +0100

    Array is plugged to the cordova plugin response

--- SimpleAroundMeIonic/src/pages/home/home.ts
+++ SimpleAroundMeIonic/src/pages/home/home.ts
@@ -1,47 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, NgZone} from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import {NavController} from 'ionic-angular';
 
-declare var cordova:any;
+declare var cordova: any;
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
-  items: string[];
+    items: string[];
 
-  constructor(public navCtrl: NavController) {
-      this.items = [
-          'Pokémon Yellow',
-          'Super Metroid',
-          'Mega Man X',
-          'The Legend of Zelda',
-          'Pac-Man',
-          'Super Mario World',
-          'Street Fighter II',
-          'Half Life',
-          'Final Fantasy VII',
-          'Star Fox',
-          'Tetris',
-          'Donkey Kong III',
-          'GoldenEye 007',
-          'Doom',
-          'Fallout',
-          'GTA',
-          'Halo'
-      ];
-  }
+    constructor(public navCtrl: NavController, private zone: NgZone) {
+        this.items = [
+            'Pokémon Yellow',
+            'Super Metroid',
+            'Mega Man X',
+            'The Legend of Zelda',
+            'Pac-Man',
+            'Super Mario World',
+            'Street Fighter II',
+            'Half Life',
+            'Final Fantasy VII',
+            'Star Fox',
+            'Tetris',
+            'Donkey Kong III',
+            'GoldenEye 007',
+            'Doom',
+            'Fallout',
+            'GTA',
+            'Halo'
+        ];
+    }
 
-  testDesEnfers() {
-      var that = this;
-      cordova.plugins.HelloPluginCordovaSwift.giveMeAnArray("PoulpinePilou",
-          function(arrayResponse) {
-              that.items = ['TUTU'];
-          },
-          function(err) {
-              alert("Swift Actually working array too " + err);
-          }
-      );
-  }
+    testDesEnfers() {
+        var that = this;
+        cordova.plugins.HelloPluginCordovaSwift.giveMeAnArray("PoulpinePilou",
+            function (arrayResponse) {
+                that.zone.run(function () {
+                        that.items = arrayResponse;
+                    }
+                )
+            },
+            function (err) {
+                alert("Swift Actually working array too " + err);
+            }
+        );
+
+    }
 }
